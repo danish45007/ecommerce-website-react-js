893b10f8dba69173cc975b3c2e9a90e9867e0d17
danish45007 <myemail>
2020-07-01 20:30:06 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Route,Switch } from 'react-router-dom'
 import HomePage from '../src/pages/homepage/homepage.component'
 import './App.css';
+
+const HatsPage = () => {
+  return(
+    <div>
+      <h1>
+        This second HomePage
+      </h1>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
