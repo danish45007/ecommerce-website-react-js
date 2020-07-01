d94ff02cb811f4a8311fa0068e72a0d9e93df05b
danish45007 <myemail>
2020-07-02 01:57:30 +0530

    added header comp <Shop> <Contact>

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Route,Switch } from 'react-router-dom'
 import HomePage from '../src/pages/homepage/homepage.component'
 import './App.css';
 import ShopPage from "./pages/shop/shop.component"
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-      </Switch>
+      <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+        </Switch>
     </div>
   );
 }
