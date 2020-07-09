7b0ea22c0077d28c1335002ffc7c943ae3519f7c
danish45007 <myemail>
2020-07-09 20:03:22 +0530

    finishing signin handlesubmit method

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
                 ...snapShot.data()
               }
             });
-            console.log(this.state)
+            // console.log(this.state)
         });
       }
       else {
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.style.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle,auth } from '../../firebase/firebase.utils';
 
 
 class SignIn extends React.Component {
@@ -15,14 +15,18 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ 
-            email: '',
-            password: ''
-         }
-        )
+        const {email, password} = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email,password)
+            this.setState({ email: '',password: ''})
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     handleChange = event => {
